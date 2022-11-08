87b6758603e287a1b95a004c1a6aaae9f391f512
Elfi_Y <29664811+elfiyang16@users.noreply.github.com>
2022-11-08 19:35:40 +0000

    refactor config

--- config.go
+++ config.go
@@ -5,12 +5,30 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/umtkas/image-resizer-lambda/configs"
+	"os"
 )
 
-func New3Downloader(configuration configs.Configuration) (*s3manager.Downloader, error) {
+const (
+	LocalDir = "/tmp"
+)
+
+type Config struct {
+	Region string
+	Bucket string
+}
+
+func NewConfig() *Config {
+	region := os.Getenv("AWS_REGION")
+	bucket := os.Getenv("AWS_BUCKET")
+	return &Config{
+		Region: region,
+		Bucket: bucket,
+	}
+}
+
+func NewS3Downloader(config *Config) (*s3manager.Downloader, error) {
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(configuration.Region),
+		Region: aws.String((*config).Region),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to init s3 session, %v", err)
@@ -19,9 +37,9 @@ func New3Downloader(configuration configs.Configuration) (*s3manager.Downloader,
 	return s3manager.NewDownloader(sess), nil
 }
 
-func NewS3Uploader(configuration configs.Configuration) (*s3manager.Uploader, error) {
+func NewS3Uploader(config *Config) (*s3manager.Uploader, error) {
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(configuration.Region),
+		Region: aws.String((*config).Region),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to init s3 session, %v", err)
